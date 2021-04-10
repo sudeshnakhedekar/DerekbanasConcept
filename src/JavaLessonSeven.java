96a04a24fd9d77ff5d05079610afd800cca6a579
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-10 16:20:58 +0530

    Sub of Monster

---
+++ src/JavaLessonSeven.java
@@ -0,0 +1,13 @@
+public class JavaLessonSeven {
+
+    public static void main(String[] args) {
+        Monster Frank = new Monster();
+
+        Frank.name ="Frank";
+
+        System.out.println(Frank.name + " has an attack of " +Frank.getAttack());
+
+        System.out.println(Frank.name + " has an attack of " +Frank.getMovement());
+
+    }
+}
