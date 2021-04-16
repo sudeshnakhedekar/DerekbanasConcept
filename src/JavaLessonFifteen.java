17c54440be142c656bd7936192657d90f6c56bb0
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-16 17:49:47 +0530

    Abstract and Interface 3/4

---
+++ src/JavaLessonFifteen.java
@@ -0,0 +1,12 @@
+public class JavaLessonFifteen {
+
+    public static void main(String[] args) {
+
+        Vehicle car = new Vehicle(4, 100.00);
+        System.out.println("Car Max Speed " +car.getSpeed());
+        System.out.println("Num of Wheels " +car.getWheel());
+
+        car.setCarStrength(10);
+        System.out.println("Strength : " +car.getCarStrength());
+    }
+}
