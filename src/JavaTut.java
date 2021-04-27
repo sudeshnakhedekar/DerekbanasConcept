f0274dd96bb301bd71816f14947dd0c94cc9144b
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-27 21:14:03 +0530

    String Function

--- src/JavaTut.java
+++ src/JavaTut.java
@@ -7,16 +7,44 @@ public class JavaTut {
 
     public static void main(String[] args) {
 
-        //Random Num
-        int minNum = 5;
-        int maxNum = 20;
-        int randNum = minNum + (int)(Math.random() *
-                ((maxNum - minNum) +1));
-        System.out.println("Rand :" + randNum);
+    //Strings
 
-        //Another rand num between  from 0 to 50
-        int randnum = (int) (Math.random() * 50);
-        System.out.println("Rand :" + randnum);
+    String name ="Sudeshna";
+    String wName =name +" Khedekar";
+    wName +=" is my name";
+        System.out.println(wName);
+        String drDog ="K" +9;
+
+        System.out.println(wName.charAt(10) );
+        System.out.println(wName.contains("sudeshna"));// prints false
+        System.out.println(wName.contains("Sudeshna"));//prints true
+        System.out.println(wName.indexOf("Sudeshna"));
+        System.out.println(wName.length());
+
+        System.out.println("dog equals cat : " +("dog").equals("cat"));
+
+        //if it gives 0 then values are same
+        //if it gives -1 then String comes before
+        //if it gives anything above String comes after
+        System.out.println(wName.compareTo("ABC"));
+
+        System.out.println(wName.replace("Sudeshna" , "Sudu"));
+
+        System.out.println(wName.substring(0,8));
+
+        for (String x: wName.split(" "))//Space after avery String word
+        System.out.println(x);
+
+        //delete the white space from the begining and ending of the String
+
+        System.out.println();
+        System.out.println("TrimFunction: ");
+        System.out.println(wName.trim().toUpperCase());
+        System.out.println(wName.trim().toLowerCase());
+
+
+
+
 
 
 
