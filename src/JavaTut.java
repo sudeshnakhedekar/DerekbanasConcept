e13b13c980042fad18e59eca6628cbedfcc9a250
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-29 10:22:36 +0530

    StringBuilder

--- src/JavaTut.java
+++ src/JavaTut.java
@@ -7,40 +7,21 @@ public class JavaTut {
 
     public static void main(String[] args) {
 
-    //Strings
+        //String builder
 
-    String name ="Sudeshna";
-    String wName =name +" Khedekar";
-    wName +=" is my name";
-        System.out.println(wName);
-        String drDog ="K" +9;
-
-        System.out.println(wName.charAt(10) );
-        System.out.println(wName.contains("sudeshna"));// prints false
-        System.out.println(wName.contains("Sudeshna"));//prints true
-        System.out.println(wName.indexOf("Sudeshna"));
-        System.out.println(wName.length());
-
-        System.out.println("dog equals cat : " +("dog").equals("cat"));
-
-        //if it gives 0 then values are same
-        //if it gives -1 then String comes before
-        //if it gives anything above String comes after
-        System.out.println(wName.compareTo("ABC"));
-
-        System.out.println(wName.replace("Sudeshna" , "Sudu"));
-
-        System.out.println(wName.substring(0,8));
-
-        for (String x: wName.split(" "))//Space after avery String word
-        System.out.println(x);
-
-        //delete the white space from the begining and ending of the String
-
-        System.out.println();
-        System.out.println("TrimFunction: ");
-        System.out.println(wName.trim().toUpperCase());
-        System.out.println(wName.trim().toLowerCase());
+        StringBuilder sb = new StringBuilder("I'm a string builder");
+        //num of char.
+        System.out.println(sb.length());
+       //size that is set aside for your StringBuilder
+        System.out.println(sb.capacity());
+        //append
+        sb.append(" Yeah");
+        System.out.println(sb.insert(6,"Big "));
+        System.out.println(sb.replace(6,9 ,"wig"));
+        System.out.println(sb.substring(6,10));
+        System.out.println(sb.delete(6,10));
+        System.out.println(sb.charAt(4));
+        System.out.println(sb.indexOf("Yeah"));
 
 
 
